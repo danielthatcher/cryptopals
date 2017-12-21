1d7fa22c3d2f1c6c8e6d88cceb4f8ebfc897c085
Daniel Thatcher <dcthatch@gmail.com>
2017-12-21 21:57:48 +0000

    Completed challenge 34

--- set5/challenge34.go
+++ set5/challenge34.go
@@ -19,6 +19,7 @@ var A big.Int
 var B big.Int
 var server Server
 
+// Server functions
 func echoKey(A big.Int) big.Int {
 	b := rand.Int()
 	B = d.PublicKey(b)
@@ -34,10 +35,28 @@ func echoMessage(msg []byte, iv []byte) ([]byte, []byte) {
 	return ciphertext, iv
 }
 
+// MitM functions
+func mitmKey(A big.Int) big.Int {
+	echoKey(d.P)
+	return d.P
+}
+
+func mitmMessage(msg []byte, iv []byte) ([]byte, []byte) {
+	// We know that the shared key (pre-hash) is going to be 0
+	var s big.Int
+	s.SetInt64(0)
+	S, _ := d.SharedKey(s, 1)
+	fmt.Println("Attacker received: ", string(util.DHDecrypt(msg, S, iv)))
+	return echoMessage(msg, iv)
+}
+
+// Client
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	d = util.NISTDiffieHellman()
 
+	// Non-MitM vesion
+	fmt.Println("Normal operation:")
 	a := rand.Int()
 	A = d.PublicKey(a)
 
@@ -47,5 +66,19 @@ func main() {
 	ciphertext, iv := util.DHEncrypt([]byte("This is a testing message"), S)
 	ciphertext, iv = echoMessage(ciphertext, iv)
 	plaintext := util.DHDecrypt(ciphertext, S, iv)
+	fmt.Println("Client received: ", string(plaintext))
+
+	// MitM version
+	fmt.Println("")
+	fmt.Println("MitM operation:")
+	a = rand.Int()
+	A = d.PublicKey(a)
+
+	mitmB := mitmKey(A)
+	S, _ = d.SharedKey(mitmB, a)
+
+	ciphertext, iv = util.DHEncrypt([]byte("This is a testing message"), S)
+	ciphertext, iv = mitmMessage(ciphertext, iv)
+	plaintext = util.DHDecrypt(ciphertext, S, iv)
 	fmt.Println("Client received: ", string(plaintext))
 }
