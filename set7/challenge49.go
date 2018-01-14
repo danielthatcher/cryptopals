85a6cf277ddaebae97b9dcd4d7a1104ae0d4de6a
Daniel Thatcher <dcthatch@gmail.com>
2018-01-14 17:43:53 +0000

    Completed challenge 49

--- set7/challenge49.go
+++ set7/challenge49.go
@@ -13,46 +13,47 @@ import (
 type API struct {
 	signedInUser int
 	secretKey    []byte
+	iv           []byte
 }
 
 func newAPI() *API {
 	rand.Seed(time.Now().UnixNano())
 	k := util.RandomBytes(16)
-	a := API{secretKey: k, signedInUser: 42}
+	a := API{secretKey: k, signedInUser: 42, iv: make([]byte, 16)}
 	return &a
 }
 
-func (api *API) generateSendRequest(toID int, amount int) string {
-	msg := fmt.Sprintf("from=%d&to=%d&amount=%d", api.signedInUser, toID, amount)
-	iv := util.RandomBytes(16)
-	mac := util.CBCMAC([]byte(msg), api.secretKey, iv)
-	return fmt.Sprintf("%s$%x$%x", msg, iv, mac)
+func (api *API) generateSendRequest(txList [][2]int) string {
+	txString := ""
+	for i := range txList {
+		if i > 0 {
+			txString = fmt.Sprintf("%s;", txString)
+		}
+		txString = fmt.Sprintf("%s%d:%d", txString, txList[i][0], txList[i][1])
+	}
+
+	msg := fmt.Sprintf("from=%d&tx_list=%s", api.signedInUser, txString)
+	mac := util.CBCMAC([]byte(msg), api.secretKey, api.iv)
+	return fmt.Sprintf("%s$%x", msg, mac)
 }
 
 func (api *API) receiveSendRequest(req string) bool {
 	parts := strings.Split(req, "$")
-	if len(parts) != 3 {
+	if len(parts) != 2 {
 		fmt.Println("Invalid request string")
 		return false
 	}
 
 	// Extract the parts
 	msg := parts[0]
-
-	iv, err := hex.DecodeString(parts[1])
-	if err != nil {
-		fmt.Println("Error decoding iv")
-		return false
-	}
-
-	mac, err := hex.DecodeString(parts[2])
+	mac, err := hex.DecodeString(parts[1])
 	if err != nil {
 		fmt.Println("Error decoding MAC")
 		return false
 	}
 
 	// Compare MAC (with a timing side channel)
-	trialMAC := util.CBCMAC([]byte(msg), api.secretKey, iv)
+	trialMAC := util.CBCMAC([]byte(msg), api.secretKey, api.iv)
 	for j := range trialMAC {
 		if trialMAC[j] != mac[j] {
 			fmt.Println("Invalid MAC")
@@ -62,47 +63,82 @@ func (api *API) receiveSendRequest(req string) bool {
 
 	// If we've gotten to this point, we can transfer the money
 	msgParts := strings.Split(msg, "&")
-	var toID, fromID, amount int
+	var fromID int
+	txList := make([][2]int, 0)
 	for i := range msgParts {
 		if strings.HasPrefix(msgParts[i], "from=") {
 			fromID, err = strconv.Atoi(strings.Split(msgParts[i], "=")[1])
 			if err != nil {
 				return false
 			}
-		} else if strings.HasPrefix(msgParts[i], "to=") {
-			toID, err = strconv.Atoi(strings.Split(msgParts[i], "=")[1])
-			if err != nil {
-				fmt.Println("Invalid to ID")
+		} else if strings.HasPrefix(msgParts[i], "tx_list=") {
+			txParts := strings.Split(msgParts[i], "=")
+			if len(txParts) != 2 {
+				fmt.Println("Invalid transaction list")
 				return false
 			}
-		} else if strings.HasPrefix(msgParts[i], "amount=") {
-			amount, err = strconv.Atoi(strings.Split(msgParts[i], "=")[1])
-			if err != nil {
-				fmt.Println("Invalid amount")
-				return false
+
+			txParts = strings.Split(txParts[1], ";")
+			for j := range txParts {
+				p := strings.Split(txParts[j], ":")
+				if len(p) != 2 {
+					fmt.Println("Invalid transaction list")
+					continue
+				}
+
+				to, err := strconv.Atoi(p[0])
+				if err != nil {
+					fmt.Println("Invalid transaction list")
+					continue
+				}
+
+				amount, err := strconv.Atoi(p[1])
+				if err != nil {
+					fmt.Println("Invalid transaction list")
+					continue
+				}
+
+				txList = append(txList, [2]int{to, amount})
 			}
 		} else {
 			fmt.Println("Invalid message")
 			return false
 		}
 	}
-	fmt.Printf("Transfering %d spacebucks from user %d to user %d\n", amount, fromID, toID)
+
+	fmt.Println("Running transactions...")
+	for i := range txList {
+		fmt.Printf("Transfering %d spacebuck from user %d to user %d\n", txList[i][1], fromID, txList[i][0])
+	}
+
 	return true
 }
 
 func main() {
 	api := newAPI()
-	req := api.generateSendRequest(42, 10000000)
-	firstBlock := req[:16]
-	req = strings.Replace(req, "from=42", "from=01", 1)
 
-	reqParts := strings.Split(req, "$")
-	ivStr := reqParts[1]
-	iv, _ := hex.DecodeString(ivStr)
+	// Capture request from victim
+	req := api.generateSendRequest([][2]int{[2]int{4, 100}, [2]int{12, 1003}})
+	fmt.Println(req)
 
-	xored := util.SliceXOR([]byte(firstBlock), []byte(req[:16]))
-	iv = util.SliceXOR(iv, xored)
-	req = fmt.Sprintf("%s$%x$%s", reqParts[0], iv, reqParts[2])
+	// We're going to get easily predictable padding before CBC, so add it
+	victimParts := strings.Split(req, "$")
+	victimMAC, _ := hex.DecodeString(victimParts[1])
+	msg := string(util.Pad([]byte(victimParts[0]), 16))
+	fmt.Println([]byte(msg))
 
+	// CBA to implement diff users, but doesn't matter here
+	attackerReq := api.generateSendRequest([][2]int{[2]int{1, 1}, [2]int{42, 1000000}})
+	attackerParts := strings.Split(attackerReq, "$")
+	attackerMAC, _ := hex.DecodeString(attackerParts[1])
+	attackerMsg := attackerParts[0]
+	attackerBlock := attackerParts[0]
+
+	attackerBlock = string(util.SliceXOR([]byte(attackerBlock), victimMAC))
+	attackerMsg = fmt.Sprintf("%s%s", attackerBlock, attackerMsg[16:])
+	msg = fmt.Sprintf("%s%s", msg, attackerMsg)
+	fmt.Println(msg)
+
+	req = fmt.Sprintf("%s$%x", msg, attackerMAC)
 	api.receiveSendRequest(req)
 }
