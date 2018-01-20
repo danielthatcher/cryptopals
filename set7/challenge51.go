d6d6f522d6f0babafd6df7ef830b1ffba7a52ec6
Daniel Thatcher <dcthatch@gmail.com>
2018-01-20 18:48:23 +0000

    Second part of challenge 51

--- set7/challenge51.go
+++ set7/challenge51.go
@@ -23,24 +23,34 @@ Content-Length: %d
 	w.Write([]byte(contents))
 	w.Close()
 
-	encrypted := make([]byte, len(b.Bytes()))
+	padded := util.Pad(b.Bytes(), 16)
+	encrypted := make([]byte, len(padded))
 	key := util.RandomBytes(16)
 	iv := util.RandomBytes(16)
 	block, _ := aes.NewCipher(key)
-	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(encrypted, []byte(b.Bytes()))
+	cbc := cipher.NewCBCEncrypter(block, iv)
+	cbc.CryptBlocks(encrypted, padded)
 
 	return len(encrypted)
 }
 
 func main() {
-	secret := []byte("sessionid=")
-	n := compressionOracle(secret)
+	rbytes := util.RandomBytes(32)
+	rbytes = append([]byte{1}, rbytes...)
+	secret := []byte("Cookie: sessionid=")
 
 	for {
+		// Find the cutoff length
+		n := compressionOracle(secret)
+		var base int
+		for base = 0; compressionOracle(append(secret, rbytes[:base+1]...)) == n; base++ {
+		}
+
 		l := len(secret)
 		for b := byte(0); b < 255; b++ {
-			if compressionOracle(append(secret, b)) <= n {
+			trialSecret := append(secret, b)
+			trialSecret = append(trialSecret, rbytes[:base]...)
+			if compressionOracle(trialSecret) <= n {
 				secret = append(secret, b)
 				fmt.Printf("\r%s", string(secret))
 				break
@@ -62,7 +72,7 @@ func main() {
 	fmt.Println("")
 	fmt.Println(string(secret))
 
-	if string(secret) == "sessionid=TmV2ZXIgcmV2ZWFsIHRoZSBXdS1UYW5nIFNlY3JldCE=" {
+	if string(secret) == "Cookie: sessionid=TmV2ZXIgcmV2ZWFsIHRoZSBXdS1UYW5nIFNlY3JldCE=" {
 		fmt.Println("Successfully found secret")
 	} else {
 		fmt.Println("Not quite...")
